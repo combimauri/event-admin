65f29371ee2ef842c1867c3997a824add2aabb79
Mauricio Arce <mauricio.arce.mat@gmail.com>
2022-11-17 23:51:54 -0400

    Added options for registration to scanner.

--- src/app/assistants/assistants.component.ts
+++ src/app/assistants/assistants.component.ts
@@ -4,20 +4,205 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { AuthService } from '../core/services/auth/auth.service';
-import { AuthUser } from '../core/models/auth-user.model';
 import { Postulant } from '../core/models/postulant.model';
 import { PostulantsService } from '../core/services/postulants.service';
+import { Ticket } from '../core/models/ticket.enum';
 
 @Component({
   selector: 'wc-assistants',
   templateUrl: './assistants.component.html',
+  styleUrls: ['./assistants.component.scss'],
 })
 export class AssistantsComponent implements OnInit, OnDestroy {
   assistants: Postulant[];
   currentAssistant: Postulant;
   currentUser$ = this.auth.getCurrentUser();
-  rfidInputEnabled = false;
   searchTerm = '';
+
+  registrationFields = {
+    [Ticket.ROJO]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackFirstDay',
+          label: 'Refrigerio',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackSecondDay',
+          label: 'Refrigerio',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+        {
+          field: 'cup',
+          label: 'Tomatodo',
+        },
+        {
+          field: 'notebook',
+          label: 'Cuaderno',
+        },
+      ],
+    },
+    [Ticket.AMARILLO]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackFirstDay',
+          label: 'Refrigerio',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackSecondDay',
+          label: 'Refrigerio',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+        {
+          field: 'bracelet',
+          label: 'Manilla',
+        },
+        {
+          field: 'notebook',
+          label: 'Cuaderno',
+        },
+      ],
+    },
+    [Ticket.VERDE]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+      ],
+    },
+    [Ticket.AZUL]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+      ],
+    },
+  };
+
+  workshops = [
+    {
+      field: 'firstWorkshop',
+      label: 'Automatiza procesos en tus Angular Apps con Github Actions',
+    },
+    {
+      field: 'secondWorkshop',
+      label:
+        'Crea tu propia inteligencia artificial con pocos conocimientos de programación',
+    },
+  ];
+
+  Ticket = Ticket;
 
   private unsubscribe$ = new Subject<void>();
 
@@ -55,19 +240,5 @@ export class AssistantsComponent implements OnInit, OnDestroy {
         (assistant) => (assistant.visibleInSearch = true),
       );
     }
-  }
-
-  enableEditingRFID(assistant: Postulant): void {
-    this.rfidInputEnabled = true;
-    this.currentAssistant = assistant;
-  }
-
-  saveAssistantRFID(currentUser: AuthUser, assistant: Postulant): void {
-    this.postulantsService.giveRFIDToPostulant(
-      currentUser,
-      assistant,
-      assistant.rfid,
-    );
-    this.rfidInputEnabled = false;
   }
 }
--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -4,10 +4,10 @@ import { Subscription, timer } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 import { AuthService } from '../core/services/auth/auth.service';
-import { AuthUser } from '../core/models/auth-user.model';
 import { PostulantsService } from '../core/services/postulants.service';
 import { Postulant } from '../core/models/postulant.model';
 import { ModalDirective } from '../shared/directives/modal/modal.directive';
+import { Ticket } from '../core/models/ticket.enum';
 
 const defaultRate = 3;
 
@@ -18,16 +18,196 @@ const defaultRate = 3;
 })
 export class ScannerComponent implements OnDestroy {
   currentUser$ = this.auth.getCurrentUser();
-  images = {
-    info: 'assets/images/processed.png',
-    error: 'assets/images/error.png',
-  };
-  modalImage = this.images.error;
   modalMessage: string;
   postulant: Postulant;
   postulantId: string;
   postulantPoints = defaultRate;
   selectedItemForScan: string;
+
+  registrationFields = {
+    [Ticket.ROJO]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackFirstDay',
+          label: 'Refrigerio',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackSecondDay',
+          label: 'Refrigerio',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+        {
+          field: 'cup',
+          label: 'Tomatodo',
+        },
+        {
+          field: 'notebook',
+          label: 'Cuaderno',
+        },
+      ],
+    },
+    [Ticket.AMARILLO]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackFirstDay',
+          label: 'Refrigerio',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+        {
+          field: 'snackSecondDay',
+          label: 'Refrigerio',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+        {
+          field: 'bracelet',
+          label: 'Manilla',
+        },
+        {
+          field: 'notebook',
+          label: 'Cuaderno',
+        },
+      ],
+    },
+    [Ticket.VERDE]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchFirstDay',
+          label: 'Almuerzo',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+        {
+          field: 'lunchSecondDay',
+          label: 'Almuerzo',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+      ],
+    },
+    [Ticket.AZUL]: {
+      firstDay: [
+        {
+          field: 'checkInFirstDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastFirstDay',
+          label: 'Desayuno',
+        },
+      ],
+      secondDay: [
+        {
+          field: 'checkInSecondDay',
+          label: 'Check In',
+        },
+        {
+          field: 'breakfastSecondDay',
+          label: 'Desayuno',
+        },
+      ],
+      souvenirs: [
+        {
+          field: 'shirt',
+          label: 'Polera',
+        },
+      ],
+    },
+  };
+
+  workshops = [
+    {
+      field: 'firstWorkshop',
+      label: 'Automatiza procesos en tus Angular Apps con Github Actions',
+    },
+    {
+      field: 'secondWorkshop',
+      label:
+        'Crea tu propia inteligencia artificial con pocos conocimientos de programación',
+    },
+  ];
+
+  Ticket = Ticket;
 
   @ViewChild('postulantProcessedModal', { static: true })
   private postulantModal: ModalDirective;
@@ -35,15 +215,15 @@ export class ScannerComponent implements OnDestroy {
   private postulantSubscription: Subscription;
 
   constructor(
+    public postulantsService: PostulantsService,
     private auth: AuthService,
-    private postulantsService: PostulantsService,
   ) {}
 
   ngOnDestroy(): void {
     this.postulantSubscription?.unsubscribe();
   }
 
-  processQRCode(postulantId: string, currentUser: AuthUser): void {
+  processQRCode(postulantId: string): void {
     if (!this.postulantId && postulantId) {
       this.postulantId = encodeURIComponent(postulantId);
 
@@ -52,15 +232,14 @@ export class ScannerComponent implements OnDestroy {
       }
 
       this.postulantSubscription = this.postulantsService
-        .getById(postulantId)
+        .getById(this.postulantId)
         .pipe(first())
         .subscribe((postulant) => {
           if (postulant) {
+            this.modalMessage = 'La lectura del credencial fue exitosa';
             this.postulant = postulant;
-            this.processScanSelection(currentUser);
           } else {
-            this.modalMessage = 'This does not look like a valid credential';
-            this.modalImage = this.images.error;
+            this.modalMessage = 'Esto no parece un credencial válido';
           }
 
           timer(100).subscribe(() => this.postulantModal.modalInstance.open());
@@ -68,99 +247,9 @@ export class ScannerComponent implements OnDestroy {
     }
   }
 
-  setPostulantRate({ rating }: { rating: number }): void {
-    this.postulantPoints = rating;
-  }
-
   cleanData(): void {
     this.postulantId = '';
     this.postulant = null;
-    this.postulantSubscription.unsubscribe();
-  }
-
-  private processScanSelection(currentUser: AuthUser): void {
-    const postulantFieldValueForSelection =
-      this.postulant[this.selectedItemForScan];
-
-    if (
-      this.selectedItemForScan === 'teachersWhoGavePoints' ||
-      !postulantFieldValueForSelection
-    ) {
-      this.processScanAccordingToField(currentUser);
-    } else {
-      this.modalMessage = 'Assistant was already checked';
-      this.modalImage = this.images.error;
-    }
-  }
-
-  private processScanAccordingToField(currentUser: AuthUser): boolean {
-    let scanCorrectly = false;
-
-    switch (this.selectedItemForScan) {
-      case 'checkIn':
-        scanCorrectly = this.postulantsService.checkInAssistant(
-          currentUser,
-          this.postulant,
-        );
-        this.modalMessage = scanCorrectly
-          ? 'Check in was correct'
-          : 'Check in could not be completed. Review if the assistant was validated';
-        break;
-      case 'feeForLunchReceived':
-        scanCorrectly = this.postulantsService.markFeeForLunchAsReceived(
-          currentUser,
-          this.postulant,
-        );
-        this.modalMessage = scanCorrectly
-          ? 'Fee for lunch was received correctly'
-          : 'Fee for lunch could not be processed. Review if the assistant made the check in';
-        break;
-      case 'lunchDelivered':
-        scanCorrectly = this.postulantsService.markLunchAsDelivered(
-          currentUser,
-          this.postulant,
-        );
-        this.modalMessage = scanCorrectly
-          ? 'Lunch was delivered correctly'
-          : 'Lunch could not be processed. Review if the assistant gave her/his fee';
-        break;
-      case 'firstSnackDelivered':
-        scanCorrectly = this.postulantsService.markFirstSnackAsDelivered(
-          currentUser,
-          this.postulant,
-        );
-        this.modalMessage = scanCorrectly
-          ? 'First snack was delivered correctly'
-          : 'First snack could not be processd. Review if the assistant made the check in';
-        break;
-      case 'secondSnackDelivered':
-        scanCorrectly = this.postulantsService.markSecondSnackAsDelivered(
-          currentUser,
-          this.postulant,
-        );
-        this.modalMessage = scanCorrectly
-          ? 'Second snack was delivered correctly'
-          : 'Second snack could not be processd. Review if the assistant made the check in';
-        break;
-      case 'teachersWhoGavePoints':
-        scanCorrectly = this.postulantsService.givePointsToPostulant(
-          currentUser,
-          this.postulant,
-          this.postulantPoints,
-        );
-        this.modalMessage = scanCorrectly
-          ? `${this.postulantPoints} ${
-              this.postulantPoints === 1 ? 'point was' : 'points were'
-            } given to ${this.postulant.fullName}`
-          : 'Something went wrong. It looks like you are not a speaker';
-        break;
-      default:
-        this.modalMessage = 'Looks like you did not choose an option';
-        break;
-    }
-
-    this.modalImage = scanCorrectly ? this.images.info : this.images.error;
-
-    return scanCorrectly;
+    this.postulantSubscription?.unsubscribe();
   }
 }
